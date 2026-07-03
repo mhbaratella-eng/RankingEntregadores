100c087f2cff4d478ed42aa530dd69bd8e7c5a3d
mhbaratella-eng <mhbaratella@gmail.com>
2026-07-03 13:36:56 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(
+    page_title="Ranking OL",
+    page_icon="🏆"
+)
+
+st.title("🏆 Ranking da OL")
+
+try:
+    df = pd.read_excel("ranking.xlsx")
+
+    df["CPF"] = df["CPF"].astype(str)
+
+except:
+    st.error("Não foi possível abrir a planilha.")
+    st.stop()
+
+cpf = st.text_input(
+    "Digite seu CPF",
+    type="password"
+)
+
+if st.button("Consultar"):
+
+    resultado = df[df["CPF"] == cpf]
+
+    if resultado.empty:
+        st.error("CPF não encontrado.")
+
+    else:
+        nome = resultado.iloc[0]["Nome"]
+        posicao = resultado.iloc[0]["Posição"]
+
+        st.success(f"Olá, {nome}!")
+
+        st.metric(
+            label="Sua posição no ranking",
+            value=f"{posicao}º"
+        )
