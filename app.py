058e5c5c2114db83550a7b32a8b19fcc97896d8d
mhbaratella-eng <mhbaratella@gmail.com>
2026-07-13 15:20:01 -0300

    Update app.py

--- app.py
+++ app.py
@@ -291,6 +291,6 @@ if st.button("Consultar"):
             )
             # Banner da promoção
 try:
-    st.image("banner.jpg", use_container_width=True)
+    st.image("banner.jpeg", use_container_width=True)
 except Exception:
     pass
