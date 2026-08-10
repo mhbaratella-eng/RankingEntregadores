367a1b5adf7c558a2fe5ce3f9f6ca72ca9edba7b
mhbaratella-eng <mhbaratella@gmail.com>
2026-08-10 16:08:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -333,12 +333,12 @@ if st.button("Consultar"):
                 )
             elif tempo_online > 75:
                 st.success(
-                    "🎉 Você está elegível ao bônus extra de R$50! "
+                    "🎉 Você está elegível ao bônus extra de mais R$50! "
                     "Seu tempo online está acima de 75%."
                 )
             else:
                 st.error(
-                    "❌ Você não está elegível ao bônus extra de R$50. "
+                    "❌ Você não está elegível ao bônus extra de mais R$50. "
                     "O tempo online precisa estar acima de 75%."
                 )
 
