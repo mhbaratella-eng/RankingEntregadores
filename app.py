dbd0bb7222e009b31d71884e49fc9c75bba5bf49
mhbaratella-eng <mhbaratella@gmail.com>
2026-08-13 17:00:55 -0300

    Update last update date and period in app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ st.set_page_config(
 # ==========================================
 
 AVISO = """
-Última atualização: 11/08/2026. Período entre 10/08 a 11/08.
+Última atualização: 12/08/2026. Período entre 10/08 a 12/08.
 
 Pagamentos sempre são realizados às sextas. 
 
